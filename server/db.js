96b6c83ff2c9f7ff9a40747c848eec847e96f42f
elyy102 <103173379+elyy102@users.noreply.github.com>
2024-02-06 18:28:34 +0500

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,9 +2,9 @@ import postgres from 'postgres'
 
 //подключение к базе данных
 export const sql = postgres({
-    host: '192.168.147.50',
+    host: 'localhost',
     port: 5432,
-    db: 'mariia1604',
-    username: 'mariia1604',
-    password: '*Cm2b8t30r3'
+    db: 'test',
+    username: 'postgres',
+    password: 'elvira.E1'
 })
