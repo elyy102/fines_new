ce2fdfe83a0a410d344c894753fec872706e57a4
elyy102 <103173379+elyy102@users.noreply.github.com>
2024-02-06 18:29:46 +0500

    Update profile_admin.jsx

--- client/src/profile_admin.jsx
+++ client/src/profile_admin.jsx
@@ -1,40 +1,65 @@
-import { useState } from 'react'
-
+import { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
 import './App.css'
 import Logo from './logo'
 import Card from './card'
 import Card_admin from './card_admin'
 
 function Profile_admin() {
-  const [count, setCount] = useState(0)
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [phone_number, setPhoneNumber] = useState('')
+  
+    const [my_info, setMyInfo] = useState([])
+  
+    const token = useSelector((state) => state.auth.token)
+
+    useEffect(() => {
+        fetch("http://localhost:3000/admin_info/", {
+          method: 'GET',
+          mode: 'cors',
+          headers: {
+            "Content-Type": "application/json",
+            'Authorization': `Bearer ${token}`
+          }
+        })
+          .then(data => data.json())
+          .then(data => setMyInfo(data))
+      }, [])
 
   return (
     <>
-    <div className="wrapper">
 
-    <Logo />
-    <div className="profile">
-        <h1 className='login_rectangle_h1'>Личный кабинет администратора</h1>
-        <div className="profile_data">
-            <div className="profile_pa">
-                <p className="profile_p">Иванов Иван Иванович</p>
-                <a href="">редактировать</a>  
-            </div>
-            <div className="profile_pa">
-                <p className="profile_p">ivanov@gmail.com</p>
-                <a href="">редактировать</a>  
-            </div>
-            <div className="profile_pa">
-                <p className="profile_p">89501856384</p>
-                <a href="">редактировать</a>  
-            </div>  
-        </div>
-        <div className="profile_myapplications">
-            <Card_admin />
-        </div>
-    </div>
+{
+        my_info.map(el =>
+        <div className="wrapper">
 
-    </div>
+        <Logo />
+        <div className="profile">
+            <h1 className='login_rectangle_h1'>Личный кабинет администратора</h1>
+            <div className="profile_data">
+                <div className="profile_pa">
+                    <p className="profile_p">{el.name}</p>
+                    <a href="">редактировать</a>  
+                </div>
+                <div className="profile_pa">
+                    <p className="profile_p">{el.email}</p>
+                    <a href="">редактировать</a>  
+                </div>
+                <div className="profile_pa">
+                    <p className="profile_p">{el.phone_number}</p>
+                    <a href="">редактировать</a>  
+                </div>  
+            </div>
+            <div className="profile_myapplications">
+                <Card_admin />
+            </div>
+        </div>
+
+        </div>
+        )
+      }
+
     </>
   )
 }
